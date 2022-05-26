37c0e8c1816f963bad9803fdab6bbbce92539245
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2022-05-26 13:16:05 +0800

    Create index.php

---
+++ modul6/index.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    include 'Koneksi2.php';
+    include 'Koneksi.php';
+
+    if (!isset($_SESSION["Login"])) {
+        header("Location: FormLogin.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <style>
+        table, td{
+            border: solid 1px black;
+            padding: 10px;
+            background-color: wheat;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <td><a href="Member.php"><button>Member</button> </a></td>
+            <td><a href="Buku.php"><button>Buku</button> </a></td>
+            <td><a href="Peminjaman.php"><button>Peminjaman</button> </a></td>  
+
+        </tr>
+</table>
+<a href="Logout.php">Logout</a>
+</body>
+</html>
