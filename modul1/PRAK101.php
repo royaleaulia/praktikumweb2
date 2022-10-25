f29df6ccc0a57e75ea09da24da1ad08f47eb702f
BandarSabu <akhiratul2002@gmail.com>
2022-10-25 21:45:31 +0700

    membahkan codingan

--- modul1/PRAK101.php
+++ modul1/PRAK101.php
@@ -1,6 +1,7 @@
 <?php 
 echo "Hello World in PHP <br>";
 echo "Nama Praktikum: Muhammad Aulia Akbar <br>";
+echo "Nama Praktikum: Akhiratu Akmal Agustiannoor <br>";
 echo "NIM : 2010817210023";
 
 ?>
